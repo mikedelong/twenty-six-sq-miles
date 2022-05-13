d29d1253a066fd34572064ff3b60e066e7bb9bf2
Mike DeLong <mikedelong@outlook.com>
2022-05-13 15:43:09 -0400

    feature: write the complete dataset to CSV

--- assessments.py
+++ assessments.py
@@ -7,26 +7,26 @@ from logging import INFO
 from logging import StreamHandler
 from logging import basicConfig
 from logging import getLogger
-from os.path import basename
 from pathlib import Path
 from sys import stdout
 
 from arrow import now
+from pandas import DataFrame
 from pandas import set_option
 from requests import get
 
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 LOG_FORMAT = '%(asctime)s.%(msecs)03d - %(levelname)s - %(name)s - %(message)s'
 LOG_PATH = Path('./logs/')
-LOG_PATH.mkdir(exist_ok=True)
 
 if __name__ == '__main__':
     time_start = now()
+    LOG_PATH.mkdir(exist_ok=True)
 
     set_option('display.max_colwidth', None)  # was -1 and caused a warning
     run_start_time = now().strftime('%Y-%m-%d_%H-%M-%S')
-    file_root_name = basename(__file__).replace('.py', '')
-    LOGFILE = str(LOG_PATH / 'log-{}-{}.log'.format(run_start_time, file_root_name))
+    file_root_name = 'assessments'
+    LOGFILE = '{}/log-{}-{}.log'.format(LOG_PATH, run_start_time, file_root_name)
 
     handlers = [FileHandler(LOGFILE), StreamHandler(stdout)]
     # noinspection PyArgumentList
@@ -35,10 +35,22 @@ if __name__ == '__main__':
     logger = getLogger()
     logger.info('started')
 
-    URL = 'https://datahub-v2.arlingtonva.us/api/RealEstate/Assessment?$top=500'
+    top = 101000
+    URL = 'https://datahub-v2.arlingtonva.us/api/RealEstate/Assessment?$top={}'.format(top)
 
     response = get(url=URL, params={})
-    data = response.json()
-    logger.info(data)
+
+    df = DataFrame.from_records(data=response.json())
+    logger.info('top: %d rows: %d', top, len(df))
+    # 'assessmentKey', 'provalLrsnId', 'realEstatePropertyCode', 'assessmentChangeReasonTypeDsc',
+    # 'assessmentDate', 'improvementValueAmt', 'landValueAmt', 'totalValueAmt'
+    for column in df.columns:
+        logger.debug('%s %s', column, df[column].unique())
+    logger.info('%d', df['realEstatePropertyCode'].nunique())
+
+    # write the DataFrame to CSV
+    output_file = './assessments.csv'
+    logger.info('writing %d rows of CSV to %s', len(df), output_file)
+    df.to_csv(path_or_buf=output_file)
 
     logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
