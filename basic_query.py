be3a184e19c8249113fe3d30217cc99d68ce719c
Mike DeLong <mikedelong@outlook.com>
2023-03-03 17:30:10 -0500

    feature: accumulate SKIP cases and make code robust to IndexErrors that indicate we need to skip (for the moment)

--- basic_query.py
+++ basic_query.py
@@ -112,57 +112,61 @@ if __name__ == '__main__':
                     sleep_period *= 2.0
                     logger.warning('read timeout: sleeping %0.2f', sleep_period)
 
-            soup = BeautifulSoup(result.text, 'html.parser')
-            body = soup.find('body')
-            site_inner = body.find_all('div', {'class': 'site-inner'})[0]
-            wrapper = site_inner.find('div', {'class': 'content-sidebar-wrap'})
-            main = wrapper.find('main')
-            article = main.find('article')
-            entry_content = article.find('div', {'class': 'entry-content'})
-            divs = entry_content.find_all('div')
-            subs = divs[3].find_all('div')
-            subdivs = subs[0].find_all('div')
-            document = dict()
-            if subdivs[3].text == '(Inactive)':
-                for index, item in enumerate(subdivs):
-                    pieces = item.text.split('\n')
-                    pieces = [' '.join(piece.split()) for piece in pieces]
-                    pieces = [piece for piece in pieces if piece]
-                    if pieces:
-                        if index == 1:
-                            document['RPC'] = pieces[0]
-                        elif index == 2:
-                            document['Address'] = pieces[0]
-                        elif index in {5, 6, 11, 12, 13, 15, 17, 18, 20, 21, 22, 24, 25, 26}:
-                            document[pieces[0]] = pieces[1]
-                        elif index == 8:
-                            document[pieces[0]] = ' '.join(pieces[1:])
-                        elif index == 9:
-                            if len(pieces) == 1:
-                                document[pieces[0]] = ''
-                            else:
+            try:
+                soup = BeautifulSoup(result.text, 'html.parser')
+                body = soup.find('body')
+                site_inner = body.find_all('div', {'class': 'site-inner'})[0]
+                wrapper = site_inner.find('div', {'class': 'content-sidebar-wrap'})
+                main = wrapper.find('main')
+                article = main.find('article')
+                entry_content = article.find('div', {'class': 'entry-content'})
+                divs = entry_content.find_all('div')
+                subs = divs[3].find_all('div')
+                subdivs = subs[0].find_all('div')
+                document = dict()
+                if subdivs[3].text == '(Inactive)':
+                    for index, item in enumerate(subdivs):
+                        pieces = item.text.split('\n')
+                        pieces = [' '.join(piece.split()) for piece in pieces]
+                        pieces = [piece for piece in pieces if piece]
+                        if pieces:
+                            if index == 1:
+                                document['RPC'] = pieces[0]
+                            elif index == 2:
+                                document['Address'] = pieces[0]
+                            elif index in {5, 6, 11, 12, 13, 15, 17, 18, 20, 21, 22, 24, 25, 26}:
                                 document[pieces[0]] = pieces[1]
-            else:
-                for index, item in enumerate(subdivs):
-                    pieces = item.text.split('\n')
-                    pieces = [' '.join(piece.split()) for piece in pieces]
-                    pieces = [piece for piece in pieces if piece]
-                    if pieces:
-                        if index == 1:
-                            document['RPC'] = pieces[0]
-                        elif index == 2:
-                            document['Address'] = pieces[0]
-                        elif index in {4, 5, 9, 10, 11, 13, 15, 16, 18, 19, 20, 22, 23, 24}:
-                            document[pieces[0]] = pieces[1]
-                        elif index == 6:
-                            document[pieces[0]] = ' '.join(pieces[1:])
-            document['LRSN'] = lrsn
-            document['fetched'] = now()
-            if lrsn in prior_df['LRSN'].values:
-                prior_df = prior_df[prior_df['LRSN'] != lrsn]
-                logger.info('dropping LRSN %d from prior', lrsn)
-            documents.append(document)
-            count += 1
+                            elif index == 8:
+                                document[pieces[0]] = ' '.join(pieces[1:])
+                            elif index == 9:
+                                if len(pieces) == 1:
+                                    document[pieces[0]] = ''
+                                else:
+                                    document[pieces[0]] = pieces[1]
+                else:
+                    for index, item in enumerate(subdivs):
+                        pieces = item.text.split('\n')
+                        pieces = [' '.join(piece.split()) for piece in pieces]
+                        pieces = [piece for piece in pieces if piece]
+                        if pieces:
+                            if index == 1:
+                                document['RPC'] = pieces[0]
+                            elif index == 2:
+                                document['Address'] = pieces[0]
+                            elif index in {4, 5, 9, 10, 11, 13, 15, 16, 18, 19, 20, 22, 23, 24}:
+                                document[pieces[0]] = pieces[1]
+                            elif index == 6:
+                                document[pieces[0]] = ' '.join(pieces[1:])
+                document['LRSN'] = lrsn
+                document['fetched'] = now()
+                if lrsn in prior_df['LRSN'].values:
+                    prior_df = prior_df[prior_df['LRSN'] != lrsn]
+                    logger.info('dropping LRSN %d from prior', lrsn)
+                documents.append(document)
+                count += 1
+            except IndexError:
+                SKIP.add(lrsn)
+                logger.info(SKIP)
             if count % 10 == 0:
                 df = DataFrame(data=documents).drop_duplicates()
                 result_df = concat([df, prior_df]).drop_duplicates(ignore_index=True)
@@ -173,5 +177,6 @@ if __name__ == '__main__':
     result_df = result_df.sort_values(by='LRSN').reset_index()
     logger.info('writing %d records to %s', len(result_df), output_file)
     result_df.to_csv(path_or_buf=output_file, index=False)
+    logger.info(sorted(list(SKIP)))
 
     logger.info('total time: {:5.2f}s'.format((now() - time_start).total_seconds()))
