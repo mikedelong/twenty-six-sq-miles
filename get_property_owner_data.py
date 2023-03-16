2f0b6c487b7a524d3a4ec654755887cdd5fe9916
Mike DeLong <mikedelong@outlook.com>
2023-03-16 16:51:55 -0400

    refactor: give this file a sensible name now that it has real functionality

--- get_property_owner_data.py
+++ get_property_owner_data.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
 
     set_option('display.max_colwidth', None)  # was -1 and caused a warning
     run_start_time = now().strftime('%Y-%m-%d_%H-%M-%S')
-    file_root_name = 'basic_query'
+    file_root_name = 'get_property_owner_data'
     LOGFILE = '{}/log-{}-{}.log'.format(LOG_PATH, run_start_time, file_root_name)
 
     handlers = [FileHandler(LOGFILE), StreamHandler(stdout)]
